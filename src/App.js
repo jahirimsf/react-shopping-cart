2636d249a77deb5200520c2aaec0d06db8742ea2
Jahir <jahir.imsf@gmail.com>
2020-10-25 18:30:55 +0600

    Add feature 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid_container">
+      <header>
+        <a href="/">React Shopping Cart</a>
       </header>
+      <main>
+        <h1>this main</h1>
+      </main>
+      <footer>this is footer</footer>
     </div>
   );
 }
